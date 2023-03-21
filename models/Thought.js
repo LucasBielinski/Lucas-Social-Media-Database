d2584cfb8a05c653323b77e2e7f7f8a0ee9f8fe8
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-03-21 14:39:05 -0400

    add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,49 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: true,
-      min_length: 1,
-      max_length: 280,
+      minlength: 1,
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
     },
-    username: {},
+    username: {
+      type: String,
+      required: true,
+    },
     reactions: [reactionSchema],
   },
   {
@@ -27,6 +58,6 @@ thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const Thought = modle("thought", thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
